2362a59c0d1919c4467aaa22bf45d0f1510488f7
yt Chen <yutongc30@gmail.com>
2024-05-30 09:51:44 +0800

    在star的函式中加入其他參數與switch的應用

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<style>
+    * {
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
 <h1>自訂函式</h1>
 <?php
 // 在頁面上快速顯示陣列內容
@@ -20,6 +25,9 @@ echo "</pre>"; */
 
 dd($a);
 dd($b);
+star();
+star('菱形', 11);
+star('矩形', 10);
 
 
 /* 
@@ -37,6 +45,50 @@ function dd($array)
         echo "</pre>";
     } else {
         echo "錯誤：函式dd()參數須為陣列";
+    }
+}
+
+function star($shape = '正三角形', $n = 7)
+{
+    switch ($shape) {
+        case "正三角形":
+        case 'equilateral triangle':
+            for ($row = 1; $row <= $n; $row++) {
+                for ($space = 1; $space <= $n - $row; $space++) {
+                    echo "&nbsp;";
+                }
+                for ($col = 1; $col <= $row * 2 - 1; $col++) {
+                    echo "*";
+                }
+                echo "<br>";
+            }
+            break;
+        case "菱形":
+        case 'diamond':
+            for ($row = 1; $row < $n * 2; $row++) {
+                if ($row > $n) {
+                    $stars = $n * 2 - $row;
+                } else {
+                    $stars = $row;
+                }
+                for ($space = 1; $space <= $n - $stars; $space++) {
+                    echo "&nbsp;";
+                }
+                for ($col = 1; $col <= $stars * 2 - 1; $col++) {
+                    echo "*";
+                }
+                echo "<br>";
+            }
+            break;
+        case "矩形":
+        case 'rectangle':
+            for ($row = 1; $row <= $n; $row++) {
+                for ($col = 1; $col <= $n; $col++) {
+                    echo "*";
+                }
+                echo "<br>";
+            }
+            break;
     }
 }
 
